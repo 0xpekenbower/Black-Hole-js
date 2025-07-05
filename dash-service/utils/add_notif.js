57f8f20103ec6d6886c7cb38a73845c67b14afd2
bel-oirg <mr_butgha@hotmail.com>
2025-07-05 05:22:50 +0000

    added producer of add users

--- dash-service/utils/add_notif.js
+++ dash-service/utils/add_notif.js
@@ -1,8 +1,25 @@
 import pool from "../config/pooling.js"
+import kafka from "../config/kafkaClient.js"
 
 const add_notif = async (accountID, otherID, notif_type) => {
     await pool.query('INSERT INTO notifs(sender, receiver, notif_type) \
         VALUES($1, $2, $3);', [accountID, otherID, notif_type])
+
+    const prod = kafka.producer()
+
+    await prod.connect()
+    await prod.send({
+        topic: 'Relation',
+        messages: [{
+            value: JSON.stringify({
+                sender: accountID,
+                receiver: otherID,
+                type: notif_type,
+                created_at: new Date()
+            })
+        }]
+    })
+    await prod.disconnect()
 }
 
 /**
